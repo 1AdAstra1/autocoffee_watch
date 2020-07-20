c3c777cfca38c59cc3fa389a1c0094a67b474a95
ad_astra <olga.the.dark@gmail.com>
2020-07-20 16:18:31 +0200

    Initial Commit

---
+++ Coffee for Olga WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Coffee for Olga WatchKit Extension
+//
+//  Created by Olga Reznikova on 20.07.2020.
+//  Copyright © 2020 Olga Reznikova. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
